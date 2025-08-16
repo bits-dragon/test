86dd2b6fd955d98f3a29f7d0f547ac133e584c03
unknown <jackwang.m483@gmail.com>
2025-08-16 09:37:18 -0700

    123

--- test.js
+++ test.js
@@ -2,10 +2,18 @@ import { WebClient } from '@slack/web-api';
 
 
 const token = 'xoxb-8840923140053-9354338297218-YxcOfPJCNfOMaM4e3tobE3k6';
-const token1 = 'xoxb-8840923140053-9365248833524-xpGdjxBnL43wEJCm7BwExLIr';
+const token1 = 'xoxb-8840923140053-9365248833524-EDMfsBYMceFEeYdNZ6oxrwX2';
 const slackclient = new WebClient(token1);
 const proxy_errorlId = 'C09B72958BS';
-const jobsId = "C09AFMB5MV1"
-const result = await slackclient.chat.postMessage({
-    channel: jobsId, text: "asdf"
-})
+const jobsId = "C09AFMB5MV1";
+(async () => {
+    try {
+        const result = await slackclient.chat.postMessage({
+            channel: jobsId,
+            text: "Hello from bot!",
+        });
+        console.log("Message sent:", result.ts);
+    } catch (err) {
+        console.error("Slack API error:", err);
+    }
+})();
