5486f979a9590833be485f0380d98dc1487e9381
unknown <jackwang.m483@gmail.com>
2025-08-17 02:47:02 -0700

    234

--- index.js
+++ index.js
@@ -609,14 +609,14 @@ app.get('/once_run', async (req, res) => {
     body: jobs1
   });
 })
+const botItem = tokens.find(bot => bot.botname == 'MyjobBot');
+const slackclient = new WebClient(botItem.token);
 app.get('/test_slack', async (req, res) => {
-  const botItem = tokens.find(bot => bot.botname == 'MyjobBot');
-  const slackclient = new WebClient(botItem.token);
-  // const result = await slackclient.chat.postMessage({
-  //   channel: botItem.channelId,
-  //   text: '',
-  // });
-  res.json({ botItem })
+  const result = await slackclient.chat.postMessage({
+    channel: botItem.channelId,
+    text: '',
+  });
+  res.json({ result })
   try {
     const botItem = tokens.find(bot => bot.botname == 'MyjobBot');
     const slackclient = new WebClient(botItem.token);
