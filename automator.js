c71a0fc11247ee493e2396fa75b6e6ab2258bce5
unknown <jackwang.m483@gmail.com>
2025-08-16 22:43:18 -0700

    123

--- automator.js
+++ automator.js
@@ -11,7 +11,7 @@ import { WebClient } from '@slack/web-api';
 
 
 const token = 'xoxb-8840923140053-9354338297218-YxcOfPJCNfOMaM4e3tobE3k6';
-const token1 = 'xoxb-8840923140053-9365248833524-EDMfsio8pBYMceFEeYdNZ6oxrwX2';
+const token1 = 'xoxb-8840923140053-9365248833524-IqL2NpxtWD1rnct4JTh8HIZY';
 const slackclient = new WebClient(token1);
 const proxy_errorlId = 'C09B72958BS';
 const jobsId = "C09AFMB5MV1"
