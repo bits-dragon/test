0bd2ece1554b16a706542c6aa95a0568c8bedf49
unknown <jackwang.m483@gmail.com>
2025-08-16 23:03:54 -0700

    123

--- test.js
+++ test.js
@@ -1,8 +1,9 @@
 import { WebClient } from '@slack/web-api';
 
+const token1 = process.env.SLACK_BOT_TOKEN; // load from env
 
 const token = 'xoxb-8840923140053-9354338297218-YxcOfPJCNfOMaM4e3tobE3k6';
-const token1 = 'xoxb-8840923140053-9365248833524-DUa8pTMiAIadVZbu0MiodUNu';
+// const token1 = 'xoxb-8840923140053-9365248833524-DUa8pTMiAIadVZbu0MiodUNu';
 const slackclient = new WebClient(token1);
 const proxy_errorlId = 'C09B72958BS';
 const jobsId = "C09AFMB5MV1";
